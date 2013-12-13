4f848d9de8b928a312679a27601a4918f71cb19e
noi narisak <noi.narisak@gmail.com>
2013-12-13 11:38:08 -0600

    Add Usage info, commandline options

--- custom/plugins/dotfiles.plugin.zsh
+++ custom/plugins/dotfiles.plugin.zsh
@@ -1,7 +1,35 @@
 function dotfiles {
+
+   if (( $# == 0 )); then
+      echo "Usage: dotfiles [-action]"
+      echo
+      echo Options:
+      echo "    -n, --dry-run    Dry run."
+      echo "    -i, --install    Apply symbolic link."
+      echo "    -u, --uninstall  Remove symbolic link."
+      echo
+      echo "Report bugs to noi.narisak@gmail.com"
+   fi
+
 	#create symbolic link
-        for f in `find . -maxdepth 1 -type f \( ! -iname "." ! -iname ".git" ! -iname ".DS_Store" ! -iname "*.txt" ! -iname "*.text*" ! -iname "*.sh" ! -iname "*.md" \)`; do
-                echo $PWD"${f#.}" $HOME"${f#.}"
-                #ln -fs $PWD"${f#.}" $HOME"${f#.}"
-        done
+   for f in `find . -maxdepth 1 -type f \( ! -iname "." ! -iname ".git" ! -iname ".DS_Store" ! -iname "*.txt" ! -iname "*.text*" ! -iname "*.sh" ! -iname "*.md" \)`; do
+
+      if [[ "$1" == "-n" ]] || [[ "$1" == "--dry-run" ]]; then
+         echo "dry-runing..."
+         echo "ln -fs" $PWD"${f#.}" $HOME"${f#.}"
+      fi      
+      
+      if [[ "$1" == "-i" ]] || [[ "$1" == "--install" ]]; then
+         echo "applying..."
+         #ln -fs $PWD"${f#.}" $HOME"${f#.}"
+      fi        
+       
+      if [[ "$1" == "-u" ]] || [[ "$1" == "--uninstall" ]]; then
+         #//TODO: Retain the list of the dotfiles and validate if they are the same on in the oh-my-zsh root.  
+         echo "removing..."
+         echo "//TODO: Implement me, uninstall"
+         echo $PWD"${f#.}" $HOME"${f#.}"
+      fi      
+      
+   done
 }
